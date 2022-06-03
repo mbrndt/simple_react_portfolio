2e6ed64f991310cc5cedf017bec7555e01e89bf1
Mareike <mareike.brandt@hotmail.de>
2022-06-03 17:19:17 +0900

    feat: set up navbar

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<Projects />} />
+          <Route path="/contact" element={<Experience />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
